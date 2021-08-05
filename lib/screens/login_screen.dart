6ed395649314146d49e91beec0890bb41783f31c
Tanishka VAswani <tanishka@pop-os.localdomain>
2021-08-05 21:32:29 +0530

    Login screen modified

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+//import 'package:google_fonts/google_fonts.dart';
+//import 'package:font_awesome/';
+import 'package:trello/pallete.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({ Key? key }) : super(key: key);
@@ -40,10 +43,78 @@ class LoginScreen extends StatelessWidget {
                     ),
                 ),
               ),
+
+              TextInputField(),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Container(
+                  height:70,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[500]!.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+
+                  child: Center(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Icon(Icons.lock, size: 28, color: kWhite,),
+                        ),
+                        hintText: 'Password',
+                        hintStyle: kBodyText,
+                      ),
+                      obscureText: true,
+                      style: kBodyText,
+                      keyboardType: TextInputType.name,
+                      textInputAction: TextInputAction.done,
+                      ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class TextInputField extends StatelessWidget {
+  const TextInputField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        height:70,
+        decoration: BoxDecoration(
+          color: Colors.grey[500]!.withOpacity(0.5),
+          borderRadius: BorderRadius.circular(16),
+        ),
+
+        child: Center(
+          child: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              prefixIcon: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Icon(Icons.card_giftcard, size: 28, color: kWhite,),
+              ),
+              hintText: 'Email',
+              hintStyle: kBodyText,
+            ),
+            style: kBodyText,
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            ),
+        ),
+      ),
     );
   }
 }
